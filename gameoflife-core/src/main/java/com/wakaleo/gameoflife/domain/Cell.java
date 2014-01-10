b9d1a0bf86261725d7e230c4d231e3c6c0ef6c99
bwalk1020 <bwalker1020@gmail.com>
2014-01-09 16:23:43 -0800

    Changes stars to pluses

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("+"), DEAD_CELL(".");
+    LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
 
@@ -26,6 +26,6 @@ public enum Cell {
     }
 
     public String getSymbol() {
-       ## return symbol;
+        return symbol;
     }
 }
